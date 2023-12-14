5bdb857a3053c621b41d302736cc5599077d9276
Mads Elkjær <madse1998@gmail.com>
2023-12-14 13:10:39 +0100

    Squashed commit of the following:
    commit 2993be6383f73c93a347201e4140a864228dd80e
    Author: Mads Elkjær <madse1998@gmail.com>
    Date:   Thu Dec 7 09:46:25 2023 +0100
    
        Sut
    
    commit 2885a439a985dda1f398a474327acb96a2988e30
    Author: Mads Elkjær <madse1998@gmail.com>
    Date:   Thu Dec 7 09:33:02 2023 +0100
    
        sut
    
    commit 3ded5238a4d73a989f03a1a06a2e7d2d99d98d24
    Author: Mads Elkjær <madse1998@gmail.com>
    Date:   Wed Dec 6 14:40:09 2023 +0100
    
        yas
    
    commit f2faf1d1a206f630bedbac4040f56fa72399da64
    Author: Mads Elkjær <madse1998@gmail.com>
    Date:   Wed Dec 6 13:01:19 2023 +0100
    
        penis
    
    commit 5c35a341fdff7f5f6efc0379b2e3a2665ca080fa
    Author: Mads Elkjær <madse1998@gmail.com>
    Date:   Wed Nov 29 15:57:56 2023 +0100
    
        yas

--- projectCode/projectCode/controller.cpp
+++ projectCode/projectCode/controller.cpp
@@ -1,49 +1,49 @@
-/* 
-* controller.cpp
-*
-* Created: 18-11-2023 19:27:13
-* Author: madse
-*/
+/*
+ * controller.cpp
+ *
+ * Created: 18-11-2023 19:27:13
+ * Author: madse
+ */
 #include "controller.h"
 
 // default constructor
 Controller::Controller()
-: uartDriver_(), x10Driver_()
+	: uartDriver_(), x10Driver_()
 {
 	windowsInSystem_ = 0;
 	numSendt_ = 1;
 }
 
-void Controller::start(debugTypes debug = AUTO) // default debugmode er AUTO
+void Controller::start(debugTypes debug = NONE) // default debugmode er NONE
 {
 	cli();
-	// tænder interrupts.
+	// tï¿½nder interrupts.
 	EICRB |= (1 << ISC41) | (1 << ISC40); // INT4 skal trigger ved stigende kant.
-	EIMSK |= (1 << INT4);                 // Tænd INT4
-	
+	EIMSK |= (1 << INT4);				  // Tï¿½nd INT4
+
 	// Bruger timer4 med
 	// prescaler 1024
 	// CTC mode.
-	// til at aktivere en interrupt hvert minut til aflæsening af 
+	// til at aktivere en interrupt hvert minut til aflï¿½sening af
 	TCCR4A = 0;
 	TCCR4B = 0;
 	TCCR4B |= (1 << WGM42) | (1 << CS42) | (1 << CS40); // CTC mode, Prescaler 1024
-	
+
 	TCNT4 = 0;
-	
+
 	// Set compare match register for 1hz increments
 	OCR4A = 15624; // = 16000000 / (1024 * 1) - 1 (must be <65536)
-	
+
 	// Enable Timer4 compare interrupt
 	TIMSK4 |= (1 << OCIE4A);
-	
-	// Global tænd for interrupts.
+
+	// Global tï¿½nd for interrupts.
 	sei();
-	
-	// Tænder output PB5. Dette er vores data strøm til x.10 protokollen.
+
+	// Tï¿½nder output PB5. Dette er vores data strï¿½m til x.10 protokollen.
 	DDRB |= (1 << PB5);
-	
-	debugMode_ = debug;
+
+	this->setDebug(debug);
 	if (this->debugMode() == COMMAND)
 	{
 		uartDriver_.transmitString("Menu:\r\n");
@@ -58,16 +58,25 @@ void Controller::interrupt()
 {
 	uint8_t nextBit = x10Driver_.getNextBit();
 	x10Driver_.transmit(nextBit);
-	
-	// Hvis debugmode er AUTO, så skal vi returnere.
-	if (this->debugMode() == AUTO) { return; }
-	
+
+	// Hvis debugmode er NONE, sï¿½ skal vi returnere.
+	if (this->debugMode() == NONE)
+		return;
+
 	// ellers skriver vi til output hvad der er sendt.
-	if (nextBit == 1) {
+	if (nextBit == 1)
+	{
 		uartDriver_.transmitString(" 1 ");
-		} else {
+	}
+	else
+	{
 		uartDriver_.transmitString(" 0 ");
 	}
+}
+
+void Controller::setDebug(debugTypes debug)
+{
+	debugMode_ = debug;
 }
 
 debugTypes Controller::debugMode()
@@ -77,98 +86,116 @@ debugTypes Controller::debugMode()
 
 void Controller::debugMenu()
 {
-	// Hvis debugmode ikke er commands, så skal vi returnere.
-	if (this->debugMode() != COMMAND) return;
+	// Hvis debugmode ikke er commands, sï¿½ skal vi returnere.
+	if (this->debugMode() != COMMAND || this->debugMode() != SEND)
+		return;
 
 	char buffer[10];
-	
-	if (!x10Driver_.dataReady()) {
-		uartDriver_.transmitString("\r\n\nKlar til næste kommando");
-		switch (uartDriver_.recieve())
+
+	if (!x10Driver_.dataReady())
+	{
+		if (this->debugMode() == SEND)
 		{
-			case 'o':
+			if (numSendt_ < 100)
 			{
 				this->sendCommandToAllWindows('O');
-				
+				numSendt_++;
+
 				sprintf(buffer, "%i", numSendt_);
 				uartDriver_.transmitString("Sendt: ");
 				uartDriver_.transmitString(buffer);
 				uartDriver_.transmitString("\r\n");
-				numSendt_++;
 			}
-			break;
-			case 'c':
-			{
-				this->sendCommandToAllWindows('C');
-			}
-			break;
-			case 'h':
-			{
-				this->sendCommandToAllWindows('H');
-			}
-			break;
-			case 'm':
-			{
-				uartDriver_.transmitString("Menu:\r\n");
-				uartDriver_.transmitString("o - Aaben vindue\r\n");
-				uartDriver_.transmitString("c - Luk vindue\r\n");
-				uartDriver_.transmitString("h - Halvt aabent\r\n");
-				uartDriver_.transmitString("m - Denne menu\r\n");
-			}
-			break;
+		}
+
+		uartDriver_.transmitString("\r\n\nKlar til nï¿½ste kommando");
+		switch (uartDriver_.recieve())
+		{
+		case 'o':
+		{
+			this->sendCommandToAllWindows('O');
+
+			sprintf(buffer, "%i", numSendt_);
+			uartDriver_.transmitString("Sendt: ");
+			uartDriver_.transmitString(buffer);
+			uartDriver_.transmitString("\r\n");
+			numSendt_++;
+		}
+		break;
+		case 'c':
+		{
+			this->sendCommandToAllWindows('C');
+		}
+		break;
+		case 'h':
+		{
+			this->sendCommandToAllWindows('H');
+		}
+		break;
+		case 'm':
+		{
+			uartDriver_.transmitString("Menu:\r\n");
+			uartDriver_.transmitString("o - Aaben vindue\r\n");
+			uartDriver_.transmitString("c - Luk vindue\r\n");
+			uartDriver_.transmitString("h - Halvt aabent\r\n");
+			uartDriver_.transmitString("m - Denne menu\r\n");
+		}
+		break;
 		}
 	}
 }
 
-
 void Controller::addWindow(uint8_t address[4])
 {
-	if (windowsInSystem_ < MAX_RECIEVERS) {
+	if (windowsInSystem_ < MAX_RECIEVERS)
+	{
 		for (uint8_t i = 0; i < 4; i++)
 		{
 			windows_[windowsInSystem_].address[i] = address[i];
 		}
-		
+
 		windowsInSystem_++;
 	}
 }
 
 void Controller::sendCommandToAllWindows(char command)
 {
-	if (debugMode_ != AUTO) { 
+	if (debugMode_ != NONE)
+	{
 		uartDriver_.transmitString("\r\nSENDER ");
 		uartDriver_.transmit(command);
 		uartDriver_.transmitString("\r\n");
 	}
-	
+
 	x10Driver_.sendData(command, windows_[0].address);
-	
-	// Nedenstående implementering har nogle disadvantages.
+
+	// Nedenstï¿½ende implementering har nogle disadvantages.
 	/*for (uint8_t i = 0; i < windowsInSystem_; i++)
 	{
 		x10Driver_.sendData(command, windows_[i].address);
 	}*/
-	
-	
 }
 
 void Controller::windowsOpen()
 {
-	if (windowState_ == 100) return;
+	if (windowState_ == 100)
+		return;
 	this->sendCommandToAllWindows('O');
 	windowState_ = 100;
 }
 
 void Controller::windowsHalf()
 {
-	if (windowState_ == 50) return;
+	if (windowState_ == 50)
+		return;
 	this->sendCommandToAllWindows('H');
 	windowState_ = 50;
 }
 
 void Controller::windowsClosed()
 {
-	if (windowState_ == 0) return;
+	if (windowState_ == 0)
+		return;
 	this->sendCommandToAllWindows('C');
 	windowState_ = 0;
 }
@@ -185,12 +212,13 @@ void Controller::printValue<int>(int value) {
 
 // Specialization for double
 template <>
-void Controller::printValue<double>(double value) {
+void Controller::printValue<double>(double value)
+{
 	// if (x10Driver_.dataReady()) {return;} // Hvis der sendes data, vil vi ikke forstyrre.
 	char buffer[50];
 	int integerPart = (int)value;
 	int decimalPart = (int)((value - integerPart) * 100);
-	
+
 	sprintf(buffer, "\r\nDouble: %d.%02d", integerPart, decimalPart);
 	uartDriver_.transmitString(buffer);
 }
--- projectCode/projectCode/main.cpp
+++ projectCode/projectCode/main.cpp
@@ -7,84 +7,93 @@
 *** Debug Modes ***
 AUTO - Automatisk mode
 COMMAND - Tillader at sende kommandoer direkte fra terminal
-WATCH - Se hvad sensoren aflæser og hvad der bliver sendt
+WATCH - Se hvad sensoren aflï¿½ser og hvad der bliver sendt
+SEND - Sender 100 O kommandoer.
 */
 #define DEBUG_MODE COMMAND
 
-
-// Hvor tit skal sensoren måle værdier? Helst over xx sekunder.
-#define MEASURE_EVERY_SECONDS 5
+// Hvor tit skal sensoren mï¿½le vï¿½rdier? Helst over xx sekunder.
+#define MEASURE_EVERY_SECONDS 30
 
 // Interrupt flags.
 volatile uint8_t interruptFlag = 0;
 volatile uint8_t measureFlag = 0;
-volatile uint8_t measureOverflow = 0; 
+volatile uint8_t measureOverflow = 0;
 
 int main(void)
 {
 	// UART uart;
-	
+
 	Controller controller;
 	controller.start(DEBUG_MODE);
-	
+
 	// Modtagerens adresse.
-	uint8_t window1[4] = {0,0,0,1};
+	uint8_t window1[4] = {0, 0, 0, 1};
 	controller.addWindow(window1);
-	
+
 	// Luftfugtighedssensor driver
 	HumiditySensorDriver humiditySensor;
-	// Hvad er MAX og MIN værdier for hvornår vinduet skal åbne?
+	// Hvad er MAX og MIN vï¿½rdier for hvornï¿½r vinduet skal ï¿½bne?
 	humiditySensor.setMaxValue(50.0);
 	humiditySensor.setMinValue(30.0);
-	const uint8_t percentDeviationToHalfOpen = 20;
-	
-	// Starter uendelig løkke.
-	while(true)
-	{	
-		// Hvis measureFlag er blevet sat, skal vi aflæse værdier fra sensoren.
-		if (measureFlag == 1) {
-			if (controller.debugMode() == WATCH) {
+	const uint8_t percentDeviationToHalfOpen = 10;
+
+	// Starter uendelig lï¿½kke.
+	while (true)
+	{
+		// Hvis measureFlag er blevet sat, skal vi aflï¿½se vï¿½rdier fra sensoren.
+		if (measureFlag == 1)
+		{
+			if (controller.debugMode() == WATCH)
+			{
 				controller.printValue(humiditySensor.readValue());
 			}
 			// uart.transmitString("MEASURE TRIGGERED\r\n");
 			double humidityProcentOff = humiditySensor.outsideLimits();
-			
-			if (humidityProcentOff < percentDeviationToHalfOpen && 0 < humidityProcentOff ) {
-				// Hvis vores measurement er under 10% af limits, så skal vi kun åbne vinduet halvt.
+
+			if (humidityProcentOff < percentDeviationToHalfOpen && 0 < humidityProcentOff)
+			{
+				// Hvis vores measurement er under 10% af limits, sï¿½ skal vi kun ï¿½bne vinduet halvt.
 				controller.windowsHalf();
-			} else if (humidityProcentOff > percentDeviationToHalfOpen) {
-				// Hvis det er mere end 10%, så åbner vi vinduet helt.
+			}
+			else if (humidityProcentOff > 10)
+			{
+				// Hvis det er mere end 10%, sï¿½ ï¿½bner vi vinduet helt.
 				controller.windowsOpen();
-			} else {
+			}
+			else
+			{
 				// Ellers luk vinduet.
 				controller.windowsClosed();
 			}
-			
+
 			// Reset flag.
 			measureFlag = 0;
 		}
-		
+
 		// Ved hver interrupt fra zero-cross detektoren kalder vi controllerens interrupt metode. Samt debug menu (hvis debug er til).
-		if (interruptFlag == 1) {
+		if (interruptFlag == 1)
+		{
 			controller.interrupt();
 			controller.debugMenu();
-			
+
 			// Reset flag.
 			interruptFlag = 0;
 		}
-		
 	}
 }
 
-ISR(INT4_vect) {
+ISR(INT4_vect)
+{
 	interruptFlag = 1;
 }
 
-ISR(TIMER4_COMPA_vect) {
+ISR(TIMER4_COMPA_vect)
+{
 	// Da timer 4 er sat til at trigger for hvert sekund (1Hz), skal vi bruge en measureOverflow integer.
 	measureOverflow++;
-	
-	// Hvis den er større end vores definerede konstant, skal vi sætte flag for at måle værdier.
+
+	// Hvis den er stï¿½rre end vores definerede konstant, skal vi sï¿½tte flag for at mï¿½le vï¿½rdier.
 	if (measureOverflow >= MEASURE_EVERY_SECONDS)
 	{
 		measureFlag = 1;
